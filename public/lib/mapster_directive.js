5ca3fdd5ac74a73bc54c085f35995d5466eab3f4
xarkes <antidepetit@gmail.com>
2016-05-19 15:45:09 +0200

    Improvements

--- public/lib/mapster_directive.js
+++ public/lib/mapster_directive.js
@@ -12,9 +12,11 @@ module.directive('vectormap', function (es, $timeout, $interval) {
 
   function link (scope, element) {
 
-    // Query every 10 seconds
-    var refresh_rate = 10;
+    var refresh_rate = 10; // Seconds
+    var current_refresh = 1000; // Ms
     var target_coords = getCoords([48.85, 2.34]);
+
+    var circles_death = [];
 
     //THIS FUNCTION IS NEEDED BECAUSE WORLD COORDS != MAP COORDS
     function getCoords(coords) {
@@ -42,68 +44,93 @@ module.directive('vectormap', function (es, $timeout, $interval) {
 
     }
 
+    function get_event_color(e) {
+      var color = {
+        'apache_404': 'red',
+        'naxsi': 'green',
+        'naxsi_learning': 'yellow',
+        'nginx_auth': 'blue',
+        'naxsi_uwa': 'purple'
+      };
+
+      return color[e["sensor"]];
+    }
+
+    function prepare_remove_circle(circle) {
+      return $timeout(function() {
+        circle.transition()
+          .attr("r", 0)
+          .duration(1000)
+          .remove();
+      }, refresh_rate * 1000);
+    }
+
     function show_event(svg, path, projection, i, event, diff) {
       $timeout(function() {
         /****** Draw it ! ******/
         var coords = getCoords(event["src_coords"].split(','));
-        var radius = 7;
 
         var circle;
         var route;
         var object;
 
-        //console.log("Origin", event["sensor"]);
+        var color = get_event_color(event);
 
-        /* Check if origin already exists */
-        if (!$("#c"+i).length) {
+        var class_ip = "ip-" + event["peer_ip"].replace(/\./g, "_");
+        circle = d3.select("." + class_ip);
+        if (circle[0][0]) {
+          // Already exists, make it bigger !
+          var size = circle.attr("r") + 1;
+          if (size > 10) size = 10;
+          circle.transition()
+            .duration(1000)
+            .attr("r", size);
+
+          // Don't die !
+          $timeout.cancel(circles_death[class_ip]);
+        } else {
+          // Create circle
           circle = svg.append("circle")
-            .attr("r", radius)
+            .attr("r", 0)
             .attr("cx", projection(coords)[0])
             .attr("cy", projection(coords)[1])
-            .attr("id", "c"+i)
-            .attr("class", "origin")
-            .style("fill", function(d) {
-              var color = {
-                'apache_404': 'red',
-                'naxsi': 'green',
-                'naxsi_learning': 'yellow',
-                'nginx_auth': 'blue'
-              };
-              console.log("Color", color[event["sensor"]]);
-              return color[event["sensor"]];
-            });
+            .attr("class", "origin " + class_ip)
+            .style("fill", color)
+            .style("stroke", "black")
+            .style("stroke-width", 1);
+
+          // Make it bigger smoothly
+          circle.transition()
+            .duration(1000)
+            .attr("r", 3);
         }
 
-        /* Check if path already exists */
-        if (!$("#p"+i).length) {
-          route = svg.append("path")
-            .datum({type: "LineString", coordinates:[coords, target_coords]})
-            .attr("class", "route")
-            .attr("d", path);
-        }
+        // Tell it to die in the future
+        circles_death[class_ip] = prepare_remove_circle(circle);
 
-        /* Check if object already exists */
-        if (!$("#o"+i).length) {
-          object = svg.append("path")
-            .attr("class", "object")
-            .attr("d", "M7.411 21.39l-4.054 2.61-.266-1.053c-.187-.744-.086-1.534.282-2.199l2.617-4.729c.387 1.6.848 3.272 1.421 5.371zm13.215-.642l-2.646-4.784c-.391 1.656-.803 3.22-1.369 5.441l4.032 2.595.266-1.053c.186-.743.085-1.533-.283-2.199zm-10.073 3.252h2.895l.552-2h-4l.553 2zm1.447-24c-3.489 2.503-5 5.488-5 9.191 0 3.34 1.146 7.275 2.38 11.809h5.273c1.181-4.668 2.312-8.577 2.347-11.844.04-3.731-1.441-6.639-5-9.156zm.012 2.543c1.379 1.201 2.236 2.491 2.662 3.996-.558.304-1.607.461-2.674.461-1.039 0-2.072-.145-2.641-.433.442-1.512 1.304-2.824 2.653-4.024z");
-        }
+        route = svg.append("path")
+          .datum({type: "LineString", coordinates:[coords, target_coords]})
+          .attr("class", "route")
+          .attr("d", path);
 
+        object = svg.append("path")
+          .style("fill", color)
+          .style("stroke", "black")
+          .style("stroke-width", 1)
+          .attr("class", "object")
+          .attr("d", "M7.411 21.39l-4.054 2.61-.266-1.053c-.187-.744-.086-1.534.282-2.199l2.617-4.729c.387 1.6.848 3.272 1.421 5.371zm13.215-.642l-2.646-4.784c-.391 1.656-.803 3.22-1.369 5.441l4.032 2.595.266-1.053c.186-.743.085-1.533-.283-2.199zm-10.073 3.252h2.895l.552-2h-4l.553 2zm1.447-24c-3.489 2.503-5 5.488-5 9.191 0 3.34 1.146 7.275 2.38 11.809h5.273c1.181-4.668 2.312-8.577 2.347-11.844.04-3.731-1.441-6.639-5-9.156zm.012 2.543c1.379 1.201 2.236 2.491 2.662 3.996-.558.304-1.607.461-2.674.461-1.039 0-2.072-.145-2.641-.433.442-1.512 1.304-2.824 2.653-4.024z");
 
         var node = route.node();
         var l = node.getTotalLength();
-        var duration = 2000;
 
         object.transition()
-          .duration(duration)
+          .duration(1500)
           .attrTween("transform", delta(node))
           .remove();
       }, diff);
     }
 
     function render() {
-
-
       element.css({
         height: element.parent().height(),
         width: '100%'
@@ -143,10 +170,9 @@ module.directive('vectormap', function (es, $timeout, $interval) {
           .attr("d", path);
       });
 
-
       $interval(function() {
         // Remove old useless elements
-        $(".origin").remove();
+
         $(".route").remove();
 
         // Get data from es
@@ -160,7 +186,7 @@ module.directive('vectormap', function (es, $timeout, $interval) {
                 must: [
                 {
                   terms: {
-                    sensor: ['apache_404', 'nginx_auth', 'naxsi', 'naxsi_learning']
+                    sensor: ['apache_404', 'nginx_auth', 'naxsi', 'naxsi_learning', 'naxsi_uwa']
                   }
                 },
                 {
@@ -182,7 +208,6 @@ module.directive('vectormap', function (es, $timeout, $interval) {
         // Compute promise
         r.then(function(result) {
           var list = result["hits"]["hits"];
-          console.log(result);
           console.log("Computing", list.length, "events.");
 
           /* Tmp */
@@ -194,10 +219,12 @@ module.directive('vectormap', function (es, $timeout, $interval) {
 
           var wsize = l-f;
           console.log("Window time size:", wsize);
+          current_refresh = wsize;
           /* Tmp */
 
           var ref_date = list[0]["_source"]["timestamp_insert"];
           ref_date = new Date(ref_date.substring(0, ref_date.length - 3));
+          console.log(ref_date);
 
           for (var i = 0; i < list.length; i++) {
             if (list[i] == undefined) {
