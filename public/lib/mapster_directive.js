54f31c2362777ac2445f58c25504e253f4efd291
xarkes <antidepetit@gmail.com>
2016-05-24 12:05:24 +0200

    Added options + special events

--- public/lib/mapster_directive.js
+++ public/lib/mapster_directive.js
@@ -18,15 +18,13 @@ module.directive('mapster', function (es, $timeout) {
     var object_box = null;
 
     /* Constants */
-    const target_coords = getCoords([48.85, 2.34]);
-    const object_form = "M7.411 21.39l-4.054 2.61-.266-1.053c-.187-.744-.086-1.534.282-2.199l2.617-4.729c.387 1.6.848 3.272 1.421 5.371zm13.215-.642l-2.646-4.784c-.391 1.656-.803 3.22-1.369 5.441l4.032 2.595.266-1.053c.186-.743.085-1.533-.283-2.199zm-10.073 3.252h2.895l.552-2h-4l.553 2zm1.447-24c-3.489 2.503-5 5.488-5 9.191 0 3.34 1.146 7.275 2.38 11.809h5.273c1.181-4.668 2.312-8.577 2.347-11.844.04-3.731-1.441-6.639-5-9.156zm.012 2.543c1.379 1.201 2.236 2.491 2.662 3.996-.558.304-1.607.461-2.674.461-1.039 0-2.072-.145-2.641-.433.442-1.512 1.304-2.824 2.653-4.024z";
-    const object_scale = 0.4;
-    const object_rotation = 90; // The object is oriented to the top
-    const object_height = 24;
+    var coords = $scope.vis.params.target_coords.replace(/ /g, "").split(',');
+    const target_coords = getCoords([parseInt(coords[0]), parseInt(coords[1])]);
+    const object_shape = $scope.vis.params.object_shape;
+    const object_scale = parseFloat($scope.vis.params.object_scale);
+    const object_rotation = parseInt($scope.vis.params.object_rotation); // The object is oriented to the top
 
-    if ($scope.open == undefined) {
-      $scope.open = true;
-    }
+    $scope.open = $scope.open || true;
 
     $scope.toggleLegend = function() {
       $scope.open = !$scope.open;
@@ -55,11 +53,7 @@ module.directive('mapster', function (es, $timeout) {
           var y = p2.y - p.y;
           var r = object_rotation - Math.atan2(-y, x) * 180 / Math.PI;
 
-          //TODO Transform origin
-          var posX = p.x;
-          var posY = p.y;
-
-          return "translate(" + posX + "," + posY + ") scale(" + object_scale + ") rotate(" + r + ")";
+          return "translate(" + p.x + "," + p.y + ") scale(" + object_scale + ") rotate(" + r + ")";
         }
       }
 
@@ -139,17 +133,63 @@ module.directive('mapster', function (es, $timeout) {
             .attr("class", "object")
             .attr("transform", "scale("+object_scale+")")
             .attr('transform', 'translate(' + width + ',' + height + ')')
-            .attr("d", object_form);
+            .attr("d", object_shape);
 
-          // Animate the object
-          container.transition()
+            // Animate the object
+            container.transition()
             .duration(1500)
             .attrTween("transform", delta(route.node()))
             .remove();
 
+            }
+        }, diff);
+    }
+
+
+    function show_special_event(event, diff) {
+      $timeout(function() {
+        var coords = geohash.decode(event["coords"]);
+        coords = getCoords([coords.lat, coords.lon]);
+
+        var circle;
+        var container;
+
+        var color = $scope.colors[event["sensor"]].color;
+
+        var class_ip = "special ip-" + event["peer_ip"].replace(/\./g, "_");
+        circle = d3.select("." + class_ip);
+        if (circle[0][0]) {
+          // Already exists, make it bigger !
+          var size = parseInt(circle.attr("r")) + 1;
+          if (size > 15) size = 15;
+          circle.transition()
+            .duration(3000)
+            .attr("r", size);
+
+          // Don't die !
+          $timeout.cancel(circles_death[class_ip]);
+        } else {
+          // Create circle
+          circle = svg.append("circle")
+            .attr("r", 0)
+            .attr("cx", projection(coords)[0])
+            .attr("cy", projection(coords)[1])
+            .attr("class", "origin " + class_ip)
+            .style("fill", color)
+            .style("stroke", "red")
+            .style("stroke-width", 2);
+
+          // Make it bigger smoothly
+          circle.transition()
+            .duration(2000)
+            .attr("r", 5);
         }
+
+        // Tell it to die in the future
+        circles_death[class_ip] = prepare_remove_circle(circle);
       }, diff);
     }
+
 
     /* Render events in the data scope */
     function render_events() {
@@ -160,7 +200,6 @@ module.directive('mapster', function (es, $timeout) {
       if (list == undefined) {
         return;
       }
-      console.log("Computing", list.length, "events.");
 
       /* Tmp */
       var f = list[0]["timestamp"];
@@ -170,7 +209,6 @@ module.directive('mapster', function (es, $timeout) {
       var wsize = l-f;
       console.log("Window time size:", wsize);
       /* Tmp */
-
 
       var ref_date = new Date(list[0]["timestamp"]);
       var last_date = ref_date;
@@ -185,7 +223,7 @@ module.directive('mapster', function (es, $timeout) {
           last_date = date;
           index = 0;
         }
-        /* Recount events with same timstamp */
+        /* Recount events with same timestamp */
         if (count == 0) {
           for (var j = i; j < list.length-1; j++) {
             if (new Date(list[j]["timestamp"]) > last_date) {
@@ -198,7 +236,11 @@ module.directive('mapster', function (es, $timeout) {
         /* Make events with same timestamp appear smoothly/distributively on 1 second */
         var diff = date - ref_date;
         diff = diff + 1000/count * index;
-        show_event(list[i], diff);
+        if (list[i]["sensor"] == $scope.vis.params.special_effects) {
+          show_special_event(list[i], diff);
+        } else {
+          show_event(list[i], diff);
+        }
         index++;
       }
     }
@@ -213,7 +255,6 @@ module.directive('mapster', function (es, $timeout) {
       var height = $element.height();
       var width = $element.width();
 
-      //TODO Compute scale automatically depending on window size
       var scale = (height/300)*100;
 
       projection = d3.geo.equirectangular()
@@ -234,7 +275,7 @@ module.directive('mapster', function (es, $timeout) {
 
       // Draw a sample object to get its size
       var object = svg.append("path")
-          .attr("d", object_form);
+        .attr("d", object_shape);
       object_box = object.node().getBBox();
       object.remove();
 
@@ -253,7 +294,6 @@ module.directive('mapster', function (es, $timeout) {
 
     // First map render is a bit postponed otherwise it does not work
     $timeout(render_map, 100);
-    console.log($scope);
 
   }
 
